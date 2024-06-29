ae85d5997ce53a6f03e404c20cae8e2824d2ab52
Apple <apple@Apples-MacBook-Air.local>
2024-06-29 10:19:38 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Higlights from './components/Higlights'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Higlights/>
+    </>
+  )
+}
+
+export default App
