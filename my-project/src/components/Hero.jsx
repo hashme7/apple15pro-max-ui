eafe7ed341b8f3de2beb0c2df7308a2664e006e9
hashme7 <hashimkakkaden@gmail.com>
2024-06-30 21:11:51 +0530

    carousel finished

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
   let [videoSrc,setVideoSrc] = useState(window.innerWidth > 760?heroVideo:smallHeroVideo);
   useGSAP(()=>{
     gsap.to('#hero',{opacity:1,delay:1.5});
+    gsap.to('#cta',{opacity:1,y:'50',delay:2})
   },[])
   const handleVideoSrc = ()=>{
     if(window.innerWidth<760){
@@ -22,7 +23,7 @@ const Hero = () => {
     }
   },[])
   return (
-    <section className='w-full nav-height bg-black relative '>
+    <section className='w-full nav-height bg-black relative'>
       <div className='h-5/6 w-full flex-center flex-col'>
         <p className='hero-title' id='hero'>iphone 15 Pro</p>
         <div className='md:w-10/12 w-9/12'>
@@ -30,8 +31,10 @@ const Hero = () => {
               <source src={videoSrc} type='video/mp4'/>
             </video>
         </div>
+      <div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
+        <a href="#highlights" className='btn'>Buy</a>
+        <p className='font-normal text-xl'>From ₹134900.00*</p>
       </div>
-      <div id='cta' className='flex flex-col items-center'>
       </div>
     </section>
   )
