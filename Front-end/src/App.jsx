c6f4f023889ba3662e9e6b952e65bb315c2a200b
hashme7 <hashimkakkaden@gmail.com>
2024-07-22 19:54:01 +0530

    three mobile view started

---
+++ Front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Higlights from './components/Higlights'
+import Model from './components/Model'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Higlights/>
+      <Model/>
+    </>
+  )
+}
+
+export default App
